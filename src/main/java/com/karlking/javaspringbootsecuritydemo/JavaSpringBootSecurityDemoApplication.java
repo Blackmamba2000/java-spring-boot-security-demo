68e459d260180e2afd64ede2b3f8c0fe1a4cb5b7
Karl King <knking85@gmail.com>
2020-11-18 07:59:15 -0600

    Initial commit

---
+++ src/main/java/com/karlking/javaspringbootsecuritydemo/JavaSpringBootSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.karlking.javaspringbootsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karlking/javaspringbootsecuritydemo/JavaSpringBootSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlking.javaspringbootsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
