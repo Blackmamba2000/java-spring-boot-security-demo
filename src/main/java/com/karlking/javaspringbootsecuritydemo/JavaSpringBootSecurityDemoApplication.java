fc7bf5086e85d3b81c8d1d9e3de8e56545cd30e4
Karl King <knking85@gmail.com>
2020-11-18 11:38:27 -0600

    Added a get RESTful api for getting students. Also, added the Spring Boot Security dependancies in the pom file.

--- src/main/java/com/karlking/javaspringbootsecuritydemo/JavaSpringBootSecurityDemoApplication.java
+++ src/main/java/com/karlking/javaspringbootsecuritydemo/JavaSpringBootSecurityDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaSpringBootSecurityDemoApplication {
+public class                    JavaSpringBootSecurityDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringBootSecurityDemoApplication.class, args);
